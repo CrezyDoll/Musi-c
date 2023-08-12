65ed2212978050bc86389ee0c739f3fee0e6a618
CrezyDoll <126459834+CrezyDoll@users.noreply.github.com>
2023-08-12 15:24:22 +0530

    Update help.py

--- SankiMusic/plugins/bot/help.py
+++ SankiMusic/plugins/bot/help.py
@@ -87,7 +87,7 @@ async def helper_cb(client, CallbackQuery, _):
     if cb == "hb9":
         if CallbackQuery.from_user.id not in SUDOERS:
             return await CallbackQuery.answer(
-                "This button is only for sudoers.", show_alert=True
+                "??? 🙄 ???", show_alert=True
             )
         else:
             await CallbackQuery.edit_message_text(
