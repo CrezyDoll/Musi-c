532d3c42dc976b9e31ad2942c282ba383131cf95
CrezyDoll <126459834+CrezyDoll@users.noreply.github.com>
2023-08-13 09:57:30 +0530

    Update logo.py

--- SankiMusic/plugins/tools/logo.py
+++ SankiMusic/plugins/tools/logo.py
@@ -3,7 +3,7 @@ from pyrogram.types import Message
 
 import io
 import os
-import config
+import SankiMusic.utilities.config import config
 import random
 import requests
 from SankiMusic import bot
