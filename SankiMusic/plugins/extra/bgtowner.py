17f52eddf88071cd0457f33b8a75c1c48e7a91f6
Sangram <136038979+OfficialSangram@users.noreply.github.com>
2023-08-19 08:28:59 +0530

    Create bgtowner.py

---
+++ SankiMusic/plugins/extra/bgtowner.py
@@ -0,0 +1,18 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from SankiMusic.utilities.config import SUPPORT_GROUP
+from SankiMusic import bot
+
+
+async def new_message(chat_id: int, message: str):
+    await bot.send_message(chat_id=chat_id, text=message)
+
+
+@bot.on_message(filters.new_chat_members)
+async def on_new_chat_members(client: Client, message: Message):
+    if (await client.get_me()).id in [user.id for user in message.new_chat_members]:
+        added_by = message.from_user.mention if message.from_user else "Bgt User"
+        title = message.chat.title
+        chat_id = message.chat.id
+        bgt = f"**ɴᴇᴡ ɢʀᴏᴜᴘ**\n\n**ɢʀᴏᴜᴘ ɪᴅ**: {chat_id} \n**ɢʀᴏᴜᴘ ɴᴀᴍᴇ**: {title} \n**ᴀᴅᴅᴇᴅ ʙʏ**: {added_by}"
+        await new_message(SUPPORT_GROUP, bgt)
