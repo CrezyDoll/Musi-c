2873be28f3f623f846dab6bce6b533ed9f64e8b5
Sangram <136038979+OfficialSangram@users.noreply.github.com>
2023-08-19 08:30:22 +0530

    Update and rename bgtowner.py to wineowner.py

--- SankiMusic/plugins/extra/wineowner.py
+++ SankiMusic/plugins/extra/wineowner.py
@@ -11,8 +11,8 @@ async def new_message(chat_id: int, message: str):
 @bot.on_message(filters.new_chat_members)
 async def on_new_chat_members(client: Client, message: Message):
     if (await client.get_me()).id in [user.id for user in message.new_chat_members]:
-        added_by = message.from_user.mention if message.from_user else "Bgt User"
+        added_by = message.from_user.mention if message.from_user else "wine User"
         title = message.chat.title
         chat_id = message.chat.id
-        bgt = f"**ɴᴇᴡ ɢʀᴏᴜᴘ**\n\n**ɢʀᴏᴜᴘ ɪᴅ**: {chat_id} \n**ɢʀᴏᴜᴘ ɴᴀᴍᴇ**: {title} \n**ᴀᴅᴅᴇᴅ ʙʏ**: {added_by}"
-        await new_message(SUPPORT_GROUP, bgt)
+        wine = f"**ɴᴇᴡ ɢʀᴏᴜᴘ**\n\n**ɢʀᴏᴜᴘ ɪᴅ**: {chat_id} \n**ɢʀᴏᴜᴘ ɴᴀᴍᴇ**: {title} \n**ᴀᴅᴅᴇᴅ ʙʏ**: {added_by}"
+        await new_message(SUPPORT_GROUP, wine)
